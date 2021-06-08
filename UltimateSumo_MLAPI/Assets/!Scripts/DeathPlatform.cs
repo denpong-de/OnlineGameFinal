2d3506e3c173e9d84a57e2665feb22db4593fad3
Denpong_Denjarukul <denpong_de@hotmail.com>
2021-06-08 11:40:50 +0700

    Winner Text now working!!

--- UltimateSumo_MLAPI/Assets/!Scripts/DeathPlatform.cs
+++ UltimateSumo_MLAPI/Assets/!Scripts/DeathPlatform.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathPlatform : MonoBehaviour
 {
     [SerializeField] GameObject gameOverCanvas;
+    Text playerName;
+
+    GameObject[] players;
+
+    private void Start()
+    {
+        playerName = gameOverCanvas.transform.GetChild(2).GetComponent<Text>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            players = GameObject.FindGameObjectsWithTag("Player");
+
+            foreach (GameObject player in players)
+            {
+                if(player.transform.name != collision.transform.name)
+                {
+                    playerName.text = player.transform.name;
+                }
+            }
+
             gameOverCanvas.SetActive(true);
         }
     }
